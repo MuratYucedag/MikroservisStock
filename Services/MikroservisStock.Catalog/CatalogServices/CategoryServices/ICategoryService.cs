666946231fd0dc5501f1e0931f97f8702c508630
MuratYucedag <yucedagmurat23@gmail.com>
2025-10-21 21:59:08 +0300

    ...

---
+++ Services/MikroservisStock.Catalog/CatalogServices/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MikroservisStock.Catalog.Dtos.CategoryDtos;
+
+namespace MikroservisStock.Catalog.CatalogServices.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id);
+    }
+}
--- Services/MikroservisStock.Catalog/Mapping/GeneralMapping.cs
+++ Services/MikroservisStock.Catalog/Mapping/GeneralMapping.cs
@@ -1,6 +1,23 @@
-﻿namespace MikroservisStock.Catalog.Mapping
+﻿using AutoMapper;
+using MikroservisStock.Catalog.Dtos.CategoryDtos;
+using MikroservisStock.Catalog.Dtos.ProductDtos;
+using MikroservisStock.Catalog.Entites;
+
+namespace MikroservisStock.Catalog.Mapping
 {
     public class GeneralMapping:Profile
     {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetCategoryByIdDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetProductByIdDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+        }
     }
 }
