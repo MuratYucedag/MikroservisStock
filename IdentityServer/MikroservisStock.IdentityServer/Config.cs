3bc3bb1738546ce1fc7496f697728018dec18258
MuratYucedag <yucedagmurat23@gmail.com>
2025-11-07 21:52:26 +0300

    ...

--- IdentityServer/MikroservisStock.IdentityServer/Config.cs
+++ IdentityServer/MikroservisStock.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -19,6 +20,67 @@ namespace MikroservisStock.IdentityServer
           new ApiResource("ResourcePayment"){Scopes={"PaymentFullPermisson"}},
           new ApiResource("ResourceMessage"){Scopes={"MessageFullPermisson"}},
           new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermisson"}},
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogReadPermisson","Reading Authority for Catalog Operations"),
+            new ApiScope("CatalogFullPermisson","Full Authority for Catalog Operations"),
+            new ApiScope("OrderFullPermisson","Full Authority for Order Operations"),
+            new ApiScope("CargoFullPermisson","Full Authority for Cargo Operations"),
+            new ApiScope("BasketFullPermisson","Full Authority for Basket Operations"),
+            new ApiScope("PaymentFullPermisson","Full Authority for Payment Operations"),
+            new ApiScope("MessageFullPermisson","Full Authority for Message Operations"),
+            new ApiScope("DiscountFullPermisson","Full Authority for Discount Operations")
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //Visitor
+            new Client
+            {
+                ClientId="MikroservisStockUserId",
+                ClientName="Mikroservis Stok Visitor User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={new Secret("mikroservisstock".Sha256())},
+                AllowedScopes={ "CatalogReadPermisson",IdentityServerConstants.LocalApi.ScopeName }
+            },
+
+            //Store Manager
+             new Client
+            {
+                ClientId="MikroservisStockStoreManagerId",
+                ClientName="Mikroservis Stok Store Manager User",
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                ClientSecrets={new Secret("mikroservisstock".Sha256())},
+                AllowedScopes={ "CatalogReadPermisson", "CatalogFullPermisson", "OrderFullPermisson", IdentityServerConstants.LocalApi.ScopeName,
+                 IdentityServerConstants.StandardScopes.Email,
+                 IdentityServerConstants.StandardScopes.OpenId,
+                 IdentityServerConstants.StandardScopes.Profile
+                 }
+             },
+
+               //Admin Manager
+             new Client
+            {
+                ClientId="MikroservisStockAdminId",
+                ClientName="Mikroservis Stok Admin User",
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                ClientSecrets={new Secret("mikroservisstock".Sha256())},
+                AllowedScopes={ "CatalogReadPermisson", "CatalogFullPermisson", "OrderFullPermisson","CargoFullPermisson","BasketFullPermisson","PaymentFullPermisson","MessageFullPermisson","DiscountFullPermisson", IdentityServerConstants.LocalApi.ScopeName,
+                 IdentityServerConstants.StandardScopes.Email,
+                 IdentityServerConstants.StandardScopes.OpenId,
+                 IdentityServerConstants.StandardScopes.Profile
+                 },
+                AccessTokenLifetime=600
+             }
         };
     }
 }
--- Services/MikroservisStock.Catalog/Program.cs
+++ Services/MikroservisStock.Catalog/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using MikroservisStock.Catalog.CatalogServices.CategoryServices;
 using MikroservisStock.Catalog.CatalogServices.ProductServices;
@@ -7,6 +8,12 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceCatalog";
+});
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
